46fdf6002965e64b1c20bea7d7330b109880fca5
Carlos Cerqueira <ce.cesc01@gmail.com>
2024-03-30 17:21:58 -0300

    feat: add lab 10 - function

--- lab10-func/counter.v
+++ lab10-func/counter.v
@@ -1,4 +1,4 @@
-module counterfunc
+module counter
 #(
   parameter integer WIDTH=5
  )
@@ -15,6 +15,20 @@ module counterfunc
 //////////////////////////////////////////////////////////////////////////////
 //TO DO: DEFINE THE COUNTER COMBINATIONAL LOGIC using FUNCTION AS INSTRUCTED//
 //////////////////////////////////////////////////////////////////////////////
+  function [WIDTH-1:0] cnt_func(
+    input rst,
+    input load,
+    input enab,
+    input [WIDTH-1:0] cnt_in,
+    input [WIDTH-1:0] cnt_out
+  );
+    reg [WIDTH-1:0] cnt_func;
+    begin
+      if (rst == 1) cnt_func = 0;
+      else if (load == 1) cnt_func = cnt_in;
+      else cnt_func = enab ? cnt_out + 1 : cnt_out;
+    end
+  endfunction
 
   always @(posedge clk)
      cnt_out <= cnt_func (rst, load, enab ,cnt_in, cnt_out); //function call
